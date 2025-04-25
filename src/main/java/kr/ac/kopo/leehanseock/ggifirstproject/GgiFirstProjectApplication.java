bc840f4c37bef29ca00ca5c9e6f41207beb98885
leehanseock <leehanseock@gmail.com>
2025-04-25 18:02:57 +0900

    개발환경 세팅

---
+++ src/main/java/kr/ac/kopo/leehanseock/ggifirstproject/GgiFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.leehanseock.ggifirstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgiFirstProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GgiFirstProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/kopo/leehanseock/ggifirstproject/GgiFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.leehanseock.ggifirstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgiFirstProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
